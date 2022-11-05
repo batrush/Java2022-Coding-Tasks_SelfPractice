a5fc625bdf9100d6616993e2b8b5c255dadb7d2a
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-11-05 21:55:05 +0100

    day 01 - task2

---
+++ task1_and_2/src/com/cydeo/dataTypes/Datatypes.java
@@ -0,0 +1,21 @@
+package com.cydeo.dataTypes;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+
+		//boolean,char,byte,short,int,long,float,double
+
+		int num = 12;
+		double number = 12.5;
+		number = -129;// int
+		
+		char character = 'C';
+
+		String str = "Can";
+		
+		boolean isTrue = true;
+
+	}
+
+}
