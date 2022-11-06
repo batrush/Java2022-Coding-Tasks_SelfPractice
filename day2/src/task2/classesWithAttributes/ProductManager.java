5cc948a35d2cb8237e81fea21f6cf348d3c3e95c
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-11-06 02:17:43 +0100

    day 02 - task2

---
+++ day2/src/task2/classesWithAttributes/ProductManager.java
@@ -0,0 +1,12 @@
+package task2.classesWithAttributes;
+
+public class ProductManager {
+	public void save(Product product) {
+		//JDBC
+		System.out.println("product is saved " + product.getName() + " - " + product.getDescription());
+	}
+	
+	public void save2(int id, String name, String description, int stockAmount, double price) {
+			
+	}
+}
