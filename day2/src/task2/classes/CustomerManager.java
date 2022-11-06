cc8a05b97f971b34626a79cb57a8e3e028eef13e
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-11-06 01:55:25 +0100

    day 02 - task2

---
+++ day2/src/task2/classes/CustomerManager.java
@@ -0,0 +1,18 @@
+package task2.classes;
+
+public class CustomerManager {
+
+    public void Add() {
+        System.out.println("customer is added");
+    }
+
+    public void Remove() {
+        System.out.println("customer is deleted");
+    }
+
+    public void Update() {
+        System.out.println("customer is updated");
+    }
+
+
+}
