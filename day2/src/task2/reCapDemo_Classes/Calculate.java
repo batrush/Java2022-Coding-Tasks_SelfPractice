c6726366da95bac07592e5ce36f3740bdd6b61de
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-11-06 02:10:07 +0100

    day 02 - task2

---
+++ day2/src/task2/reCapDemo_Classes/Calculate.java
@@ -0,0 +1,22 @@
+package task2.reCapDemo_Classes;
+
+public class Calculate {
+
+
+	public int sum(int sayi1, int sayi2) {
+		return sayi1+sayi2;
+	}
+	
+	public int minus(int sayi1, int sayi2) {
+		return sayi1-sayi2;
+	}
+	
+	public int multiple(int sayi1, int sayi2) {
+		return sayi1*sayi2;
+	}
+	
+	public int divide(int sayi1, int sayi2) {
+		return sayi1/sayi2;
+	}
+	
+}
---
+++ day2/src/task2/reCapDemo_Classes/Main.java
@@ -0,0 +1,15 @@
+package task2.reCapDemo_Classes;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Calculate calculate = new Calculate();
+        int result = calculate.sum(3, 4);
+        System.out.println(result);
+
+    }
+
+
+}
